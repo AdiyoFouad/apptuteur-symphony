8c9e03720ce875493b8b6c87f3bb7eecb7c19139
AdiyoFouad <odjfouad38@gmail.com>
2025-11-19 15:57:18 +0100

    Finalisation TP5

--- src/Controller/FormController.php
+++ src/Controller/FormController.php
@@ -2,6 +2,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Twig\Environment;
@@ -13,15 +14,33 @@ class FormController extends AbstractController {
 
     public function listes(Environment $twig) {
         $tuteurs = [
-            "tuteurs" => [
-                ['nom' => "Johnson", 'prenom' => "Paul"],
-                ['nom' => "Walberg", 'prenom' => "Mark"]
-            ]
+            ['nom' => "Johnson", 'prenom' => "Paul"],
+            ['nom' => "Walberg", 'prenom' => "Mark"]   
         ];
         
-        $html=$twig->render('tuteurs.html.twig', ['tuteurs' => $tuteurs['tuteurs']] );
+        $html=$twig->render('tuteurs.html.twig', ['tuteurs' => $tuteurs] );
         return new Response ($html);
 
+    }
+
+    public function search(Environment $twig) {
+        $html = $twig->render('search_tuteur.html.twig');
+        return new Response($html);
+    }
+
+    public function verify(Environment $twig, Request $request) {
+        $tuteurs = [
+            ['nom' => "Johnson", 'prenom' => "Paul"],
+            ['nom' => "Walberg", 'prenom' => "Mark"]   
+        ];
+        $nom = htmlspecialchars($request->request->get('nom'));
+        $existe = in_array($nom, $tuteurs);
+
+        $html = $twig->render('result_tuteur.html.twig', [
+            'nom' => $nom,
+            'existe' => $existe
+        ]);
+        return new Response($html);
     }
 }
 
